d09591093e566354d1edf77b24745a574839a01b
lSleipnir <55943913+lSleipnir@users.noreply.github.com>
2021-02-08 15:51:48 +0100

    Ajout compte

--- src/banque/entites/Compte.java
+++ src/banque/entites/Compte.java
@@ -2,7 +2,7 @@ package banque.entites;
 
 public class Compte {
 
-    private double numero_compte;
+    private int numero_compte;
     private double solde_du_compte;
 
     public Compte(int numero_compte, double solde_du_compte) {
