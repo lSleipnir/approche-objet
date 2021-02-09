15bcc6b8964cdbaeec4319badae7310ae7655292
lSleipnir <55943913+lSleipnir@users.noreply.github.com>
2021-02-09 09:33:48 +0100

    maj

--- src/banque/entites/Compte.java
+++ src/banque/entites/Compte.java
@@ -2,11 +2,19 @@ package banque.entites;
 
 public class Compte {
 
-    private int numero_compte;
+    private static int i = 1;   //faut mettre static car sinon il le reload par object. static: lié à la classe et non à l'object
+    // variable static ne dépend pas de l'instance de la classe
+    private int numero_de_compte;
     private double solde_du_compte;
 
-    public Compte(int numero_compte, double solde_du_compte) {
-        this.numero_compte = numero_compte;
+    //à chaque création de compte, définir automatiquement un numéro de compte incrémenté (1, 2, 3 ..)
+    public Compte(double solde_du_compte) {
         this.solde_du_compte = solde_du_compte;
+        i++;
+        this.numero_de_compte = i;
+    }
+
+    public int getNumero_compte() {
+        return numero_de_compte;
     }
 }
--- src/banque/entites/TestBanque.java
+++ src/banque/entites/TestBanque.java
@@ -2,11 +2,22 @@ package banque.entites;
 
 public class TestBanque {
     public static void main(String[] args) {
-        Compte my_compte = new Compte(7229830, 67856735);
-        System.out.println(my_compte);
+        Compte my_compte1 = new Compte(7229330);
+        Compte my_compte2 = new Compte(722430);
+        Compte my_compte3 = new Compte(7223830);
+
+        System.out.println(my_compte1);
+        System.out.println(my_compte2);
+        System.out.println(my_compte3);
+        //Que constatez-vous ?
+        //On voit qu'on affiche l'emplacement mémoire de l'instance de l'object de la classe
+
+        System.out.println(my_compte1.getNumero_compte());
+        System.out.println(my_compte2.getNumero_compte());
+        System.out.println(my_compte3.getNumero_compte());
+
     }
 
-    //Que constatez-vous ?
-    //On voit qu'on affiche l'emplacement mémoire de l'instance de l'object de la classe
+
 
 }
